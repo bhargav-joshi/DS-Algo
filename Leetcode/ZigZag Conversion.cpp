19a704a0bf22a72aa87966be4370bac9ddc02bd4
Bhargav Joshi <47782249+bhargav-joshi@users.noreply.github.com>
2021-06-14 10:28:44 +0530

    ZigZag Conversion added

---
+++ Leetcode/ZigZag Conversion.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    string convert(string input, int numrows) {
+        string output ="";
+        
+        int len = input.length();
+        
+        if (numrows >= len || numrows ==1)
+            return input;
+        
+        // creating parallel array with zigzag seq
+        int seq[len];
+        // printing the direction
+        bool rev = false;
+        
+        for (int i=0,j=0;i<len;i++)
+        {
+            seq[i]= j;
+            if (j == numrows -1)
+                rev = true;
+            if (j == 0)
+                rev = false;
+            if (rev)
+                j--;
+            else
+                j++;
+        }
+        
+        
+        for (int j = 0, k = 0; j< numrows; j++)
+        {
+            for ( int i=0; i <len; i++)
+            {
+                if (seq[i]==j)
+                    output += input[i];
+            }
+        }
+        return output;
+    }
+};
