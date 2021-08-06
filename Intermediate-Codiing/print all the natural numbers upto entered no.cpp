6926992e31d83abd4133fd3daccbfba840a3b3f5
Bhargav Joshi <47782249+bhargav-joshi@users.noreply.github.com>
2021-08-06 20:14:17 +0530

    Added

---
+++ Intermediate-Codiing/print all the natural numbers upto entered no.cpp
@@ -0,0 +1,16 @@
+//Write a program to accept an integer from the user, and print all the natural numbers upto that integer
+#include<iostream>
+using namespace std;
+int main ()
+{
+
+  int i, N;
+  cout << "Enter the number:";
+  //user input 
+  cin >> N;
+  for (i = 0; i <= N; i++)
+  {
+      cout<<i<<" ";
+  }
+  return 0;
+}
